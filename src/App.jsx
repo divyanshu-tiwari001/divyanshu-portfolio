a3c1e55be32bb24dacab954c949a725ff08714a0
Divyanshu Tiwari <programmingwithdt@gmail.com>
2026-03-28 01:21:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const CodecademyIcon = ({ className }) => (
 
 const FEATURE_FLAGS = {
   SHOW_PROJECTS: true, // Set to false to hide the Projects section
-  SHOW_PARTICLES: false, //SGA Particles feature flag
+  SHOW_PARTICLES: true, //SGA Particles feature flag
 };
 
 export default function PremiumStudentPortfolio() {
