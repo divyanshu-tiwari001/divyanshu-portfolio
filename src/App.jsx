f8a1f87db58c8e0d5259548299ac4c1544002a03
Divyanshu Tiwari <programmingwithdt@gmail.com>
2026-04-10 23:22:04 +0530

    Disable testimonials section with feature flag

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const CodecademyIcon = ({ className }) => (
 const FEATURE_FLAGS = {
   SHOW_PROJECTS: true, // Set to false to hide the Projects section
   SHOW_PARTICLES: true, //SGA Particles feature flag
-  SHOW_TESTIMONIALS: true, // Set to false to hide the Testimonials section
+  SHOW_TESTIMONIALS: false, // Set to false to hide the Testimonials section
 };
 
 export default function PremiumStudentPortfolio() {
